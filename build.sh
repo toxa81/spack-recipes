3ebe9f2be536487642c42e7129c9b7f1047b3435
antonk <antonk@cscs.ch>
2023-06-27 18:38:20 +0200

    save

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-WORKDIR=$(pwd)
+source env.sh
 
-source $WORKDIR/spack/share/spack/setup-env.sh
-
-export SPACK_USER_CONFIG_PATH=$WORKDIR/config
-
-module load cray spack-config-generator
-
-spack -e ./env concretize -f
-spack -e ./env install
+spack -e ./env/sirius concretize -f
+spack -e ./env/sirius install
 
 
--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 #git clone --depth 1 https://github.com/spack/spack.git
 
-WORKDIR=$(pwd)
-
-source $WORKDIR/spack/share/spack/setup-env.sh
-
-mkdir -p $WORKDIR/{install,config}
-
-export SPACK_USER_CONFIG_PATH=$WORKDIR/config
+source env.sh
 
 spack config add config:install_tree:root:$WORKDIR/install
 spack config add packages:all:variants:cuda_arch=80
 
 spack external find --not-buildable git perl cpio m4 autoconf automake pkgconf curl sqlite tar xz zip bzip2
+
+spack compiler find /usr/bin
+
+spack install gcc@11.4.0 %gcc@7
+
+spack compiler find `spack location -i gcc@11.4.0`
